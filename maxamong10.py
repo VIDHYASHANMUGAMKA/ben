3d693b603b3eae8cc8eee02c852d9e7af06774cc
VIDHYASHANMUGAMKA <37236109+VIDHYASHANMUGAMKA@users.noreply.github.com>
2019-02-06 14:07:44 +0530

    Update and rename swapno.c to maxamong10.py

---
+++ maxamong10.py
@@ -0,0 +1,20 @@
+ num1 = float(input("Enter first number: "))
+ num2 = float(input("Enter second number: "))
+  num3 = float(input("Enter third number: "))
+   num4 = float(input("Enter first number: "))
+   num5 = float(input("Enter first number: "))
+   num6 = float(input("Enter first number: ")) 
+  num7 = float(input("Enter first number: "))
+  num8 = float(input("Enter first number: "))
+  num9 = float(input("Enter first number: "))
+  num10 = float(input("Enter first number: "))
+  
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number between",num1,",",num2,"and",num3,"is",largest) 
